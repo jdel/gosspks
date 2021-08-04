7af2d2cd81c6f28dec0ca8c7d8c5193aab37d342
Julien Del Piccolo <julien@del-piccolo.com>
2021-08-04 21:35:29 +0200

    Upgrade to 20.04

--- scripts/b2.sh
+++ scripts/b2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
-pip3 install --ignore-installed b2
+pyenv global 3.7
+sudo pip3 install --upgrade pip
+sudo pip3 install --ignore-installed b2
 b2 authorize-account $B2_APPLICATION_KEY_ID $B2_APPLICATION_KEY
 for binary in out/*; do
     echo "b2 upload-file --info commit=${TRAVIS_COMMIT:0:8} jdel-builds ${binary} ${TRAVIS_REPO_SLUG#jdel/}/${TRAVIS_BRANCH}/${binary#out/}"
