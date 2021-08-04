f7a0e143ad17b399cf4d99da97861fb649a7ed5f
Julien Del Piccolo <julien@del-piccolo.com>
2021-08-04 21:16:11 +0200

    Try without sudo

--- scripts/b2.sh
+++ scripts/b2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
-sudo pip3 install --upgrade pip
-sudo pip3 install --ignore-installed b2
+pip3 install --upgrade pip
+pip3 install --ignore-installed b2
 b2 authorize-account $B2_APPLICATION_KEY_ID $B2_APPLICATION_KEY
 for binary in out/*; do
     echo "b2 upload-file --info commit=${TRAVIS_COMMIT:0:8} jdel-builds ${binary} ${TRAVIS_REPO_SLUG#jdel/}/${TRAVIS_BRANCH}/${binary#out/}"
